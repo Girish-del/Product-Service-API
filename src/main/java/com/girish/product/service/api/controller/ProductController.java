1e0e271acc610e0a9229d1fda1081fcc1fb6a1f2
Girish/del <girishnalawade01@gmail.com>
2024-09-02 04:24:45 +0000

    Created createProduct and getALlProduct endpoints

--- src/main/java/com/girish/product/service/api/controller/ProductController.java
+++ src/main/java/com/girish/product/service/api/controller/ProductController.java
@@ -1,16 +1,30 @@
 package com.girish.product.service.api.controller;
 
-import com.SpringProject.product_service.dto.ProductRequest;
+import com.girish.product.service.api.dto.ProductRequest;
+import com.girish.product.service.api.dto.ProductResponse;
+import com.girish.product.service.api.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/product")
+@RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void createProduct(@RequestBody ProductRequest productRequest){
+        productService.createProduct(productRequest);
+    }
 
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts(){
+        return productService.getAllProducts();
     }
 }
--- src/main/java/com/girish/product/service/api/repository/ProductRepository.java
+++ src/main/java/com/girish/product/service/api/repository/ProductRepository.java
@@ -1,6 +1,6 @@
 package com.girish.product.service.api.repository;
 
-import com.SpringProject.product_service.model.Product;
+import com.girish.product.service.api.model.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface ProductRepository extends MongoRepository<Product, String > {
